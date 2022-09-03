83345310bff6968ad593658092243afb823e5192
RicardoArch <ricardomoar160899@gmail.com>
2022-09-03 15:18:02 -0500

    colisiones fijas

--- src/game.js
+++ src/game.js
@@ -13,6 +13,11 @@ const playerPosition = {
     y: undefined,
 }
 
+const giftPosition = {
+    x: undefined,
+    y: undefined,
+}
+
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
 
@@ -57,6 +62,9 @@ function startGame(){
                 playerPosition.y = posY;
                 console.log({playerPosition});
             }
+         } else if (col == 'I'){
+             giftPosition.x = posX;
+             giftPosition.y = posY;
          }
 
          game.fillText(emoji, posX, posY);
@@ -67,6 +75,15 @@ function startGame(){
 }
 
 function movePlayer(){
+
+    const giftCollisionX = playerPosition.x.toFixed(3) == giftPosition.x.toFixed(3);
+    const giftCollisionY = playerPosition.y.toFixed(3) == giftPosition.y.toFixed(3);
+    const giftCollision = giftCollisionX && giftCollisionY;
+
+    if (giftCollision){
+        console.log('Subiste de nivel compa');
+    }
+
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
 }
 
