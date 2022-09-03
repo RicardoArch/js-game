76005c2814b34f7dbf5c964476dd21740bd6068d
RicardoArch <ricardomoar160899@gmail.com>
2022-09-03 17:49:58 -0500

    sistema de tiempo y puntaje

--- src/game.js
+++ src/game.js
@@ -5,11 +5,16 @@ const btnLeft = document.querySelector('#left');
 const btnRight = document.querySelector('#right');
 const btnDown = document.querySelector('#down');
 const spanLives = document.querySelector('#lives');
+const spanTime = document.querySelector('#time');
 
 let canvasSize;
 let elementsSize;
 let level = 0;
 let lives = 3;
+
+let timeStart;
+let timePlayer;
+let timeInterval;
 
 const playerPosition = {
     x: undefined,
@@ -53,6 +58,11 @@ function startGame(){
     if (!map){
         gameWin();
         return;
+    }
+
+    if (!timeStart){
+        timeStart = Date.now();
+        timeInterval = setInterval(showTime, 100);
     }
 
     const mapRows = map.trim().split('\n');
@@ -133,6 +143,7 @@ function levelFail(){
     if (lives <= 0){
         level = 0;
         lives = 3;
+        timeStart = undefined;
     } 
     playerPosition.x = undefined;
     playerPosition.y = undefined;
@@ -141,6 +152,7 @@ function levelFail(){
 
 function gameWin(){
     console.log('Terminaste el juego');
+    clearInterval(timeInterval);
 }
 
 function showLives(){
@@ -150,6 +162,10 @@ function showLives(){
     spanLives.innerHTML = "";
     heartsArray.forEach(heart => spanLives.append(heart));
     
+}
+
+function showTime(){
+   spanTime.innerHTML = Date.now() - timeStart;
 }
 
 window.addEventListener('keydown', moveByKeys);
