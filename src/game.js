5189d03b6d83f2bdadf8e4366fe8d3c528ff7441
RicardoArch <ricardomoar160899@gmail.com>
2022-09-03 15:42:14 -0500

    colisiones con arrays

--- src/game.js
+++ src/game.js
@@ -18,6 +18,8 @@ const giftPosition = {
     y: undefined,
 }
 
+let enemyPositions = [];
+
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
 
@@ -47,7 +49,8 @@ function startGame(){
     const mapRows = map.trim().split('\n');
     const mapRowCols = mapRows.map(row => row.trim().split(''));
     console.log({map, mapRows});
-
+    
+    enemyPositions = [];
     game.clearRect(0,0, canvasSize, canvasSize);
 
    mapRowCols.forEach((row, rowI ) => {
@@ -65,9 +68,14 @@ function startGame(){
          } else if (col == 'I'){
              giftPosition.x = posX;
              giftPosition.y = posY;
-         }
-
-         game.fillText(emoji, posX, posY);
+         } else if (col == 'X'){
+             enemyPositions.push({
+                x: posX,
+                y: posY,
+             });
+        }
+         
+        game.fillText(emoji, posX, posY);
          
        });
    });
@@ -82,6 +90,17 @@ function movePlayer(){
 
     if (giftCollision){
         console.log('Subiste de nivel compa');
+    }
+
+    const enemyCollision = enemyPositions.find(enemy => {
+        const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
+        const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
+
+        return enemyCollisionX && enemyCollisionY;
+    });
+
+    if (enemyCollision){
+        console.log('Chocaste contra un enemigo compa');
     }
 
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
