cadca1f0bf218a9eb0cfe878d327b5ed0e162c80
RicardoArch <ricardomoar160899@gmail.com>
2022-09-03 14:35:50 -0500

    no salirse del mapa

--- src/game.js
+++ src/game.js
@@ -89,21 +89,41 @@ function moveByKeys(event){
 }
 function moveUp(){
     console.log('me quiero mover para arriba');
-    playerPosition.y -= elementsSize;
-    startGame();
+
+    if (( playerPosition.y - elementsSize ) < elementsSize){
+        console.log('OUT');
+    }else{
+        playerPosition.y -= elementsSize;
+        startGame();
+    }
 }
 function moveLeft(){
     console.log('me quiero mover para la izquierda');
-    playerPosition.x -= elementsSize;
-    startGame();
+
+    if (( playerPosition.x - elementsSize ) < elementsSize){
+        console.log('OUT');
+    }else{
+        playerPosition.x -= elementsSize;
+        startGame();
+    }
 }
 function moveRight(){
     console.log('me quiero mover para la derecha');
-    playerPosition.x += elementsSize;
-    startGame();
+
+    if (( playerPosition.x + elementsSize ) > canvasSize){
+        console.log('OUT');
+    }else{
+        playerPosition.x += elementsSize;
+        startGame();
+    }
 }
 function moveDown(){
     console.log('me quiero mover para abajo');
-    playerPosition.y += elementsSize;
-    startGame();
+    
+    if (( playerPosition.y + elementsSize ) > canvasSize){
+        console.log('OUT');
+    }else{
+        playerPosition.y += elementsSize;
+        startGame();
+    }
 }
